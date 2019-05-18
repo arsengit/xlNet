4678e964ffac3cc4d170465a456339bffdc8e074
arsengit <alkamyan97@gmail.com>
2019-05-18 15:34:42 +0400

    menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,51 +6,60 @@ import DataPicker from "./dataPicker"
 import { Animated } from "./animated"
 const logo = require("../images/home/logo.png")
 
-export interface HeaderProps {}
+export interface HeaderProps { }
 
-export interface HeaderState {
-  toggleMenu: boolean
+export interface HeaderState
+{
+  showNav: boolean
   background: boolean
   height: boolean
   show: boolean
   hamburger: boolean
+  inverseAnimation: any
 }
 
 class Header extends React.Component<HeaderProps, HeaderState> {
-  constructor(props: HeaderProps) {
+  constructor(props: HeaderProps)
+  {
     super(props)
     this.state = {
-      toggleMenu: false,
+      inverseAnimation: false,
+      showNav: false,
       background: true,
       height: false,
       show: false,
       hamburger: false,
     }
   }
-
-  closeModal = () => {
+  private _timeOutId: number | any
+  closeModal = () =>
+  {
     this.setState({
       show: !this.state.show,
     })
   }
 
-  toggleShow = (e: React.SyntheticEvent) => {
+  toggleShow = (e: React.SyntheticEvent) =>
+  {
     e.preventDefault()
     this.setState({
       show: !this.state.show,
     })
   }
 
-  handleScroll = () => {
+  handleScroll = () =>
+  {
     const docHeight = document.body.scrollHeight
     const scrollHeight = window.scrollY
     const diff = document.body.scrollHeight - window.innerHeight
     const main = diff + scrollHeight
-    if (main + 150 >= docHeight) {
+    if (main + 150 >= docHeight)
+    {
       this.setState({
         height: true,
       })
-    } else {
+    } else
+    {
       this.setState({
         height: false,
       })
@@ -58,116 +67,157 @@ class Header extends React.Component<HeaderProps, HeaderState> {
     this.changeActive()
   }
 
-  onResize = () => {
-    const small = window.innerWidth <= 800
+  onResize = () =>
+  {
+    const small = window.innerWidth <= 800;
     {
       this.setState(
         state =>
           state.hamburger === small
             ? null
             : {
-                hamburger: small,
-              },
-        () => console.log(this.state.hamburger)
+              hamburger: small,
+              showNav: !small
+            }
       )
     }
   }
 
-  menuToggle = () => {
-    this.setState({
-      toggleMenu: true,
-    })
+  menuToggle = () =>
+  {
+    if (this.state.showNav)
+    {
+      this.setState({
+        inverseAnimation: !this.state.inverseAnimation
+      }, () =>
+        {
+          if (this._timeOutId)
+          {
+            clearTimeout(this._timeOutId)
+          }
+          if (this.state.inverseAnimation)
+          {
+            this._timeOutId = setTimeout(() =>
+            {
+              this.setState({
+                showNav: false,
+                inverseAnimation: false
+              })
+            }, 700)
+          }
+
+        })
+
+    } else
+    {
+      this.setState({
+        showNav: !this.state.showNav
+      }
+      );
+    }
+
   }
 
-  changeActive = () => {
+  changeActive = () =>
+  {
     const location = window.location.pathname
-    if (location == "/" && this.state.height === true) {
+    if (location == "/" && this.state.height === true)
+    {
       this.setState({
         background: true,
       })
-    } else if (location == "/" && this.state.height === false) {
+    } else if (location == "/" && this.state.height === false)
+    {
       this.setState({
         background: false,
       })
     }
   }
 
-  componentDidMount() {
+  componentDidMount()
+  {
+    this.setState({
+      showNav: window.innerWidth > 800,
+      hamburger: window.innerWidth <= 800,
+    });
     const location = window.location.pathname
-    if (location == "/") {
+    if (location == "/")
+    {
       this.setState({
         background: !this.state.background,
       })
     }
-    if (location == "/" && this.state.height === true) {
+    if (location == "/" && this.state.height === true)
+    {
       this.setState({
         background: true,
-      })
-    }
-    if (window.innerWidth <= 800) {
-      this.setState({
-        hamburger: true,
       })
     }
     addEventListener("scroll", this.handleScroll)
     addEventListener("resize", this.onResize)
   }
 
-  componentWillUnmount() {
+  componentWillUnmount()
+  {
+    if (this._timeOutId)
+    {
+      clearTimeout(this._timeOutId);
+    }
     removeEventListener("scroll", this.handleScroll)
     removeEventListener("resize", this.onResize)
   }
 
-  render() {
-    const { show, hamburger, toggleMenu } = this.state
+  render()
+  {
+    const { show, hamburger, showNav, background, inverseAnimation } = this.state
     return (
       <div className="header-main ">
-        <div
-          className={
-            this.state.background
-              ? "header-main-bg"
-              : "header-main-bg head-opacity"
-          }
+        <div className={background ? "header-main-bg" : "header-main-bg head-opacity"
+        }
         />
 
         <header className="container">
           <Link to="/">
             <img src={logo} alt="logo" />
           </Link>
-          {hamburger && <button onClick={this.menuToggle}>Open Nav bar</button>}
-          <Animated
-            from={
-              this.state.toggleMenu
-                ? { opacity: 0, transform: "translateY(-10%)" }
-                : { opacity: 0 }
-            }
-            to={
-              this.state.toggleMenu
-                ? { opacity: 1, transform: "translateY(100%)" }
-                : { opacity: 1 }
-            }
-            inverse={this.state.hamburger}
-          >
-            {style =>
-              toggleMenu ? (
-                <ul style={style} className={hamburger ? "hamburger" : ""}>
-                  <Link activeClassName="head-active" to="/">
-                    Development
+          {hamburger && <button onClick={this.menuToggle}>Open</button>}
+
+          {
+            showNav ?
+              <Animated
+                from={
+                  showNav && hamburger ? { transform: "translateY(-100%)" } :
+                    { opacity: 0 }
+                }
+                to={
+                  showNav && hamburger ? { transform: "translateY(93%)" }
+                    : { opacity: 1 }
+                }
+                inverse={inverseAnimation}
+
+              >
+
+                {style => (
+                  <ul style={style} className={hamburger ? "hamburger" : ""}>
+                    <Link activeClassName="head-active" to="/">
+                      Development
                   </Link>
-                  <Link activeClassName="head-active" to="/pricing">
-                    Pricing
+                    <Link activeClassName="head-active" to="/pricing">
+                      Pricing
                   </Link>
-                  <Link activeClassName="head-active" to="/career">
-                    Career
+                    <Link activeClassName="head-active" to="/career">
+                      Career
                   </Link>
-                  <Link activeClassName="head-active" to="/contact">
-                    Contact
+                    <Link activeClassName="head-active" to="/contact">
+                      Contact
                   </Link>
-                  <button onClick={this.toggleShow}>Pick a Time</button>
-                </ul>
-              ) : null
-            }
-          </Animated>
+                    <button onClick={this.toggleShow}>Pick a Time</button>
+                  </ul>
+                )
+                }
+              </Animated> : null
+
+          }
         </header>
 
         <Modal onClose={this.closeModal} show={show}>
