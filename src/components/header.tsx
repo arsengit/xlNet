f68b55ff4f7cb544ab71b4377108f1b2784ba5aa
a.alkamyan <arsenalkamyan8@gmail.com>
2019-06-03 01:28:28 +0400

    fixed navBar bug

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,7 +13,7 @@ export interface HeaderState
   showNav: boolean
   background: boolean
   height: boolean
-  show: boolean
+  showModal: boolean
   hamburger: boolean
   inverseAnimation: any
 }
@@ -27,23 +27,107 @@ class Header extends React.Component<HeaderProps, HeaderState> {
       showNav: false,
       background: true,
       height: false,
-      show: false,
+      showModal: false,
       hamburger: false,
     }
   }
   private _timeOutId: number | any
-  closeModal = () =>
+
+  componentDidMount()
   {
     this.setState({
-      show: !this.state.show,
-    })
+      showNav: window.innerWidth > 900,
+      hamburger: window.innerWidth <= 900,
+    });
+    const location = window.location.pathname
+    if (location == "/")
+    {
+      this.setState({
+        background: !this.state.background,
+      })
+    }
+    if (location == "/" && this.state.height === true)
+    {
+      this.setState({
+        background: true,
+      })
+    }
+    addEventListener("scroll", this.handleScroll)
+    addEventListener("resize", this.onResize)
+  }
+
+  componentWillUnmount()
+  {
+    if (this._timeOutId)
+    {
+      clearTimeout(this._timeOutId);
+    }
+    removeEventListener("scroll", this.handleScroll)
+    removeEventListener("resize", this.onResize)
+  }
+
+  render()
+  {
+    console.log(this.state.showNav)
+    const { showModal, hamburger, showNav, background, inverseAnimation } = this.state
+    return (
+      <div className="header-main ">
+        <div className={background ? "header-main-bg" : "header-main-bg head-opacity"
+        }
+        />
+
+        <header className="container">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+          {hamburger && <button onClick={this.menuToggle}>Open</button>}
+
+          {
+            showNav &&
+            <Animated
+              from={
+                hamburger ? { transform: "translateY(-100%)" } : { opacity: 0 }
+              }
+              to={
+                hamburger ? { transform: "translateY(93%)" } : { opacity: 1 }
+              }
+              inverse={inverseAnimation}
+
+            >
+
+              {style => (
+                <ul style={style} className={hamburger ? "hamburger" : "navWithout"}>
+                  <Link activeClassName="head-active" to="/">
+                    Development
+                  </Link>
+                  <Link activeClassName="head-active" to="/pricing">
+                    Pricing
+                  </Link>
+                  <Link activeClassName="head-active" to="/career">
+                    Career
+                  </Link>
+                  <Link activeClassName="head-active" to="/contact">
+                    Contact
+                  </Link>
+                  <button onClick={this.toggleShow}>Pick a Time</button>
+                </ul>
+              )
+              }
+            </Animated>
+          }
+        </header>
+
+        <Modal onClose={this.toggleShow} show={showModal}>
+          {showModal ? <DataPicker show={showModal} /> : null}
+        </Modal>
+      </div>
+    )
   }
 
   toggleShow = (e: React.SyntheticEvent) =>
   {
-    e.preventDefault()
     this.setState({
-      show: !this.state.show,
+      showModal: !this.state.showModal,
     })
   }
 
@@ -78,7 +162,7 @@ class Header extends React.Component<HeaderProps, HeaderState> {
             : {
               hamburger: small,
               showNav: !small
-            }
+            }, () => console.log(this.state.hamburger, this.state.showNav)
       )
     }
   }
@@ -132,99 +216,6 @@ class Header extends React.Component<HeaderProps, HeaderState> {
         background: false,
       })
     }
-  }
-
-  componentDidMount()
-  {
-    this.setState({
-      showNav: window.innerWidth > 900,
-      hamburger: window.innerWidth <= 900,
-    });
-    const location = window.location.pathname
-    if (location == "/")
-    {
-      this.setState({
-        background: !this.state.background,
-      })
-    }
-    if (location == "/" && this.state.height === true)
-    {
-      this.setState({
-        background: true,
-      })
-    }
-    addEventListener("scroll", this.handleScroll)
-    addEventListener("resize", this.onResize)
-  }
-
-  componentWillUnmount()
-  {
-    if (this._timeOutId)
-    {
-      clearTimeout(this._timeOutId);
-    }
-    removeEventListener("scroll", this.handleScroll)
-    removeEventListener("resize", this.onResize)
-  }
-
-  render()
-  {
-    const { show, hamburger, showNav, background, inverseAnimation } = this.state
-    return (
-      <div className="header-main ">
-        <div className={background ? "header-main-bg" : "header-main-bg head-opacity"
-        }
-        />
-
-        <header className="container">
-          <Link to="/">
-            <img src={logo} alt="logo" />
-          </Link>
-          {hamburger && <button onClick={this.menuToggle}>Open</button>}
-
-          {
-            showNav ?
-              <Animated
-                from={
-                  showNav && hamburger ? { transform: "translateY(-100%)" } :
-                    { opacity: 0 }
-                }
-                to={
-                  showNav && hamburger ? { transform: "translateY(93%)" }
-                    : { opacity: 1 }
-                }
-                inverse={inverseAnimation}
-
-              >
-
-                {style => (
-                  <ul style={style} className={hamburger ? "hamburger" : ""}>
-                    <Link activeClassName="head-active" to="/">
-                      Development
-                  </Link>
-                    <Link activeClassName="head-active" to="/pricing">
-                      Pricing
-                  </Link>
-                    <Link activeClassName="head-active" to="/career">
-                      Career
-                  </Link>
-                    <Link activeClassName="head-active" to="/contact">
-                      Contact
-                  </Link>
-                    <button onClick={this.toggleShow}>Pick a Time</button>
-                  </ul>
-                )
-                }
-              </Animated> : null
-
-          }
-        </header>
-
-        <Modal onClose={this.closeModal} show={show}>
-          {show ? <DataPicker show={show} /> : null}
-        </Modal>
-      </div>
-    )
   }
 }
 
